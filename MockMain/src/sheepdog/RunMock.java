2e0cfd87b890335d013d7df1b4616133caf3ca8b
Ivory <IvoryEC@gmail.com>
2019-06-18 19:48:33 -0400

    Some more refactoring, little changes.

--- MockMain/src/sheepdog/RunMock.java
+++ MockMain/src/sheepdog/RunMock.java
@@ -9,11 +9,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import org.apache.commons.io.FileUtils;
 import biolockj.Config;
 import biolockj.Constants;
 import biolockj.util.BioLockJUtil;
+import biolockj.util.SummaryUtil;
 
 public class RunMock
 {
@@ -28,6 +28,7 @@ public class RunMock
 	private static final String BLJ_JAR="${BLJ}/dist/BioLockJ.jar";
 	private static final String MOCK_JAR="${SHEP}/MockMain/dist/MockMain.jar";
 	private static final String TMP_PROJ="${SHEP}/MockMain/pipelines";
+	private static final String NOT_IN_GIT="NOT_IN_GIT";
 	
 	private static class TestInfoRow {
 		File config;
@@ -63,7 +64,7 @@ public class RunMock
 			for (String s: outputHeader) {
 				row.add( getField(s) );
 			}
-			return( String.join( Constants.TAB_DELIM, row ) + Constants.RETURN );
+			return( String.join( Constants.TAB_DELIM, row ) + System.lineSeparator() );
 		}
 	}
 
@@ -71,9 +72,10 @@ public class RunMock
 			CONFIG_FILE_COL, PIPELINE_COL, NUM_COMPLETE_MODS_COL,
 			EXPECTED_OUTCOME_COL, OUTCOME_SEEN_COL, PASS_FAIL, NOTES_COL) );
 	
-	private static HashMap<Integer, TestInfoRow> tests = new HashMap<Integer, TestInfoRow>();
+	private static ArrayList<TestInfoRow> tests = new ArrayList<TestInfoRow>();
 	
 	public static void main( String args[] ){
+		long startTime = System.currentTimeMillis();
 		int surprises = 0;
 		String outFileName = args.length > 1 ? args[ 1 ] : createOutFileName( args[ 0 ] );
 		try{
@@ -82,21 +84,23 @@ public class RunMock
 			System.err.println( "Test results will be written to: " + outFileName );
 			final BufferedWriter writer = new BufferedWriter( new FileWriter( new File( outFileName ) ) );
 			writeComments(writer);
-			writer.write( String.join( Constants.TAB_DELIM, outputHeader ) + Constants.RETURN );
+			writer.write( String.join( Constants.TAB_DELIM, outputHeader ) + System.lineSeparator() );
 			
-			for( Integer id = 1; id < tests.size(); id++ ){
-				runMockBljMain( id );
-				System.err.println( "This pipeline completed " + getCompletedModuleCount( id ) + " modules.");
-				if( tests.get( id ).out_exp.equals( tests.get( id ).result ) ){
-					tests.get( id ).passes = true;
+			clearPipelines();
+			
+			for( TestInfoRow test : tests ){
+				System.err.println("=====");
+				runMockBljMain( test );
+				System.err.println( "This pipeline completed " + getCompletedModuleCount( test ) + " modules.");
+				if( test.out_exp.equals( test.result ) ){
+					test.passes = true;
 				}else {
-					tests.get( id ).passes = false;
+					test.passes = false;
 					surprises++;
-					System.err.println( "HEY! This didn't give me what I expected: " + tests.get( id ).config.getName() );
+					System.err.println( "SURPRISE! This didn't give me what I expected: " + test.config.getName() );
 				}
 				
-				writer.write( tests.get( id  ).toString() );
-				System.err.println("Added " + id + "th row to output.");
+				writer.write( test.toString() );
 			}
 			
 			writer.close();
@@ -106,15 +110,15 @@ public class RunMock
 			e.printStackTrace();
 			System.exit( 1 );
 		}
-		System.err.println( "DONE!" );
+		System.err.println( System.lineSeparator() + "DONE!" + System.lineSeparator() );
 		System.err.println( "We had: " + surprises + " surprises." );
-		System.err.println( "Test results written to: " + outFileName );
+		System.err.println( "Total test runtime: " + SummaryUtil.getRunTime( System.currentTimeMillis() - startTime ) );
 		System.exit( 0 );
 	}
 	
 	protected static String createOutFileName(String inFile) {
 		File inF = new File(inFile);
-		String name = "NOT_IN_GIT_" + inF.getName().replaceAll( "[.].{3}$", "" ) + "_results.txt";
+		String name = inF.getName().substring( 0, inF.getName().lastIndexOf( "." ) ) + "_results_" + NOT_IN_GIT + ".txt" ;
 		return (new File(inF.getParentFile(), name)).getAbsolutePath();
 	}
 	
@@ -125,7 +129,6 @@ public class RunMock
 		int iConfig = 0;
 		int iExpected = 0;
 		int iNotes = 0;
-		Integer key = new Integer( 0 );
 		BufferedReader reader = BioLockJUtil.getFileReader( inFile );
 
 		try
@@ -135,20 +138,17 @@ public class RunMock
 				final String[] cells = line.split( Constants.TAB_DELIM, -1 );
 				final ArrayList<String> row = new ArrayList<String>( Arrays.asList( cells ) );
 				
-				if( key.equals( 0 ) )
-				{
+				if( iNotes==0 ){
 					iConfig = row.indexOf( CONFIG_FILE_COL );
 					iExpected = row.indexOf( EXPECTED_OUTCOME_COL );
 					iNotes = row.indexOf( NOTES_COL );
+				}else {
+					TestInfoRow oneTest = new TestInfoRow();
+					oneTest.config = new File( Config.replaceEnvVar( row.get( iConfig ) ) );
+					oneTest.out_exp = row.get( iExpected );
+					oneTest.notes = row.get( iNotes );
+					tests.add( oneTest );
 				}
-
-				TestInfoRow oneTest = new TestInfoRow();
-				oneTest.config = new File(Config.replaceEnvVar(row.get( iConfig )));
-				oneTest.out_exp = row.get( iExpected );
-				oneTest.notes = row.get( iNotes );
-				tests.put( key, oneTest );
-
-				key = key + 1;
 			}
 		}
 		finally
@@ -158,10 +158,10 @@ public class RunMock
 		System.err.println("Read in list of " + (tests.size())  + " tests");
 	}
 	
-	protected static void runMockBljMain (Integer id) throws Exception {
+	protected static void runMockBljMain (TestInfoRow test) throws Exception {
 		String cmd = "java -cp " + Config.replaceEnvVar(MOCK_JAR) + ":" + Config.replaceEnvVar(BLJ_JAR) 
 		+ " sheepdog.MockMain " + "-b " + Config.replaceEnvVar(TMP_PROJ) 
-		+ " -u ~ -c " + tests.get( id ).config.getAbsolutePath();
+		+ " -u ~ -c " + test.config.getAbsolutePath();
 		System.err.println(cmd);
 		String result = "";
 		String pipeline = "NA";
@@ -177,7 +177,8 @@ public class RunMock
 				}
 				if( s.contains( HACKERS_PIPELINE_KEY ) )
 				{
-					pipeline = s.replaceFirst( ".*" + HACKERS_PIPELINE_KEY, "" ).trim();
+					int end = s.indexOf( HACKERS_PIPELINE_KEY ) + HACKERS_PIPELINE_KEY.length();
+					pipeline = s.substring( end + 1 );
 					System.err.println("Created pipeline dir: " + pipeline);
 				}
 				if( s.startsWith( MockMain.PIPELINE_KEY ) )
@@ -191,9 +192,8 @@ public class RunMock
 			System.err.println( "Problem occurred running command: " + cmd);
 			ex.printStackTrace();
 		}
-		tests.get( id ).result = result ;
-		tests.get( id ).pipeline = pipeline ;
-		System.err.println( "Finished running: " + tests.get(id).config.getName() );
+		test.result = result ;
+		test.pipeline = pipeline ;
 	}
 	
 	protected static void writeHeader( BufferedWriter writer ) throws IOException{
@@ -204,8 +204,8 @@ public class RunMock
 		
 	}
 	
-	private static String getCompletedModuleCount( Integer id ) {
-		File top = new File(tests.get( id ).pipeline);
+	private static String getCompletedModuleCount( TestInfoRow test ) {
+		File top = new File(test.pipeline);
 		if (top.exists() && top.isDirectory()) {
 			File[] subdirs = top.listFiles(File::isDirectory);
 			int completed = 0;
@@ -217,7 +217,7 @@ public class RunMock
 					}
 				}
 			}
-			tests.get( id ).completedModules = completed;
+			test.completedModules = completed;
 			return(Integer.toString( completed ));
 		}else {
 			return( "NA" );
@@ -225,8 +225,8 @@ public class RunMock
 	}
 	
 	protected static void writeComments(BufferedWriter writer) throws IOException {
-		writer.write( "# BioLockJ jar file: " + Config.replaceEnvVar(BLJ_JAR) );
-		writer.write( "# SHEP_DATA: " + Config.replaceEnvVar("${SHEP_DATA}").replaceAll( (".*" + File.pathSeparator ), "" ) );
+		writer.write( "# BioLockJ jar file: " + Config.replaceEnvVar(BLJ_JAR) + System.lineSeparator() );
+		writer.write( "# SHEP_DATA: " + (new File(Config.replaceEnvVar("${SHEP_DATA}"))).getName() + System.lineSeparator());
 	}
 	
 	protected static void clearPipelines() throws IOException {
