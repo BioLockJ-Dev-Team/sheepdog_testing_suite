30c59c2cbb5c2114b37c697f2098fc22688598e1
Ivory <IvoryEC@gmail.com>
2019-06-19 16:31:09 -0400

    There is only one runMockMain.sh; it takes a test list argument.

--- MockMain/resources/runMockMain.sh
+++ MockMain/resources/runMockMain.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 
+# use:
+# runMockMain exampleTestList.txt
+
 TESTER_JAR=${SHEP}/MockMain/dist/MockMain.jar
 BLJ_JAR=${BLJ}/dist/BioLockJ.jar
-IN_FILE=${SHEP}/MockMain/resources/exampleTestList.txt
-OUT_FILE=${SHEP}/MockMain/resources/exampleOutputFile.txt 
+
+if [ "$#" -eq 1 ]; then
+	IN_FILE=$1
+else
+	echo "Oh dear.  runMockMain reqires one argument giving the path to the test list."
+	exit 1
+fi
 
 # make a horizontal line in the console
 N=$(tput cols); T=$(seq 1 $N); for i in $T; do printf "="; done;
 
-java -cp ${TESTER_JAR}:${BLJ_JAR} sheepdog.RunMock ${IN_FILE} ${OUT_FILE}
+java -cp ${TESTER_JAR}:${BLJ_JAR} sheepdog.RunMock ${IN_FILE}
