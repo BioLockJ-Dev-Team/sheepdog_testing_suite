510d025707b9e22b5b253f8662923825a5285157
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-06-20 19:55:29 -0400

    test written (but not passing...)

--- MockMain/src/sheepdog/modules/RdpReparse.java
+++ MockMain/src/sheepdog/modules/RdpReparse.java
@@ -1,14 +1,23 @@
 package sheepdog.modules;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import biolockj.Log;
 import biolockj.module.BioModuleImpl;
 import biolockj.util.ModuleUtil;
+import parsers.NewRDPNode;
+import parsers.NewRDPParserFileLine;
 
 public class RdpReparse extends BioModuleImpl
 {
+	//todo : Get these from the property file
+	private static final int RDP_THRESHOLD = 80;
+	private static final String GENUS_LEVEL = "genus";
 
 	@Override
 	public void checkDependencies() throws Exception
@@ -44,11 +53,56 @@ public class RdpReparse extends BioModuleImpl
 		for(File rdpFile : rdpOutput)
 		{
 			File biolockJSummary = findMatchingSummaryFile(rdpFile, biolockJSummaryFiles);
-			Log.info(getClass(), "Getting ready to parse " + biolockJSummary.getAbsolutePath());
+			Log.info(getClass(), "Getting ready to parse " + rdpFile.getAbsolutePath());
+			HashMap<String, Long> expectationMap = getCountsForALevel(rdpFile, GENUS_LEVEL);
+			assertEqual(biolockJSummary, expectationMap, GENUS_LEVEL);
 		}
 		
-		Log.info( getClass(), "Exiting RdpReparse module");
+		Log.info( getClass(),	 "Exiting RdpReparse module");
 		
+	}
+	
+	private static void assertEqual( File inFile, HashMap<String,Long> innerMap, String level ) throws Exception
+	{
+		System.out.println("check " + inFile.getAbsolutePath());
+		BufferedReader reader = new BufferedReader(new FileReader(inFile));
+		
+		int checked =0;
+		
+		for(String s= reader.readLine(); s!= null; s= reader.readLine() )
+		{
+			StringTokenizer sToken = new StringTokenizer(s, "\t");
+			
+			if( sToken.countTokens() != 2)
+				throw new Exception("Expecting two tokens");
+			
+			String name = sToken.nextToken();
+			
+			if( name.toLowerCase().indexOf("unclassified") == -1)
+			{
+				name = name.substring(name.indexOf(level + "__"), name.length());
+				name = name.replace(level + "__", "");
+				
+				Long parsedVal = innerMap.get(name);
+				
+				if( parsedVal == null)
+					throw new Exception("Could not find " + name);
+				
+				Long thisVal = Long.parseLong(sToken.nextToken());
+				
+				if( ! thisVal.equals(parsedVal))
+					throw new Exception("Mismatched for " + name + " " + parsedVal + " " + thisVal);
+				
+				checked++;
+				
+				innerMap.remove(name);
+			}
+		}
+		
+		System.out.println("Ok checked " + checked);
+		
+		if( innerMap.size() != 0)
+			System.out.println("COULD NOT FIND  " + innerMap);
 	}
 	
 	private static File findMatchingSummaryFile( File rdpOutputFile,  List<File> biolockJSummaryFiles) throws Exception
@@ -72,6 +126,39 @@ public class RdpReparse extends BioModuleImpl
 			throw new Exception("Could not find " + sampleID );
 		
 		return returnFile;
+		
+	}
+	
+	private static HashMap<String, Long> getCountsForALevel( File aFile , String level) throws Exception
+	{
+		HashMap<String, Long> returnMap = new HashMap<>();
+		
+		HashMap<String, NewRDPParserFileLine> map = NewRDPParserFileLine.getAsMapFromSingleThread(aFile);
+		
+		for(String s : map.keySet())
+		{
+			NewRDPParserFileLine aLine = map.get(s);
+			
+			NewRDPNode aNode = aLine.getTaxaMap().get(level);
+			
+			if( aNode != null && aNode.getScore() > RDP_THRESHOLD )
+			{
+				String taxaName = aNode.getTaxaName();
+				
+				Long aLong = returnMap.get(taxaName);
+				
+				if( aLong == null)
+					aLong = 0l;
+				
+				aLong = aLong + 1;
+				
+				returnMap.put(taxaName, aLong);
+			
+			}
+			
+		}
+		
+		return returnMap;
 		
 	}
 	
