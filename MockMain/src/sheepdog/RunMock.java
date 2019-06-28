790c2eda622b21f841c471b90260014ff4e881c8
Ivory <IvoryEC@gmail.com>
2019-06-28 02:50:24 -0400

    Run Docker Tests - first draft of incorporating docker testing functionality into MockMain

--- MockMain/src/sheepdog/RunMock.java
+++ MockMain/src/sheepdog/RunMock.java
@@ -3,7 +3,6 @@ package sheepdog;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,12 +11,15 @@ import java.util.Arrays;
 import org.apache.commons.io.FileUtils;
 import biolockj.Config;
 import biolockj.Constants;
+import biolockj.Properties;
 import biolockj.util.BioLockJUtil;
 import biolockj.util.SummaryUtil;
 
 public class RunMock
 {
 	private static final String CONFIG_FILE_COL = "ConfigFile";
+	private static final String FLAGS_COL = "Flags";
+	private static final String ENV_COL = "Environment";
 	private static final String PIPELINE_COL = "PipelineDirectory";
 	private static final String NUM_COMPLETE_MODS_COL = "NumberCompletedModules";
 	private static final String EXPECTED_OUTCOME_COL = "ExpectedOutcome";
@@ -27,22 +29,33 @@ public class RunMock
 	private static final String HACKERS_PIPELINE_KEY = "Set Config property [ internal.pipelineDir ] = ";
 	private static final String BLJ_JAR="${BLJ}/dist/BioLockJ.jar";
 	private static final String MOCK_JAR="${SHEP}/MockMain/dist/MockMain.jar";
+	private static final String MOCK_DIST="${SHEP}/MockMain/dist";
 	private static final String TMP_PROJ="${SHEP}/MockMain/pipelines";
 	private static final String NOT_IN_GIT="NOT_IN_GIT";
+	private static final String DOCKER = "docker";
 	
 	private static class TestInfoRow {
 		File config;
+		String flags;
+		String environment;
 		String pipeline;
 		int completedModules;
 		String out_exp;
-		boolean passes;
 		String result;
+		boolean passes;
 		String notes;
+		String inputDir;
 		
 		String getField(String colName){
 			switch (colName) {
 				case CONFIG_FILE_COL:
 					return( config.getAbsolutePath() );
+				case FLAGS_COL:
+					return( flags );
+				case ENV_COL:
+					//only return the environment value if that affected how the test was run
+					if (environment.equals( DOCKER ) ) return( environment );
+					return( "" );
 				case PIPELINE_COL:
 					return( pipeline );
 				case NUM_COMPLETE_MODS_COL:
@@ -59,6 +72,27 @@ public class RunMock
 			return null;
 		}
 		
+		public void extractInputDir() throws Exception
+		{
+			System.err.println("Extracting input.dirPaths value from: " + config.getName() );
+			Properties props = Properties.loadProperties( config );
+			inputDir = Config.replaceEnvVar((String) props.get( "input.dirPaths" ));
+			File input = new File(inputDir);
+			if (!input.exists() || !input.isDirectory()) {
+				throw new Exception("To run in docker, input.dirPaths should give a single valid directory."
+						+ "\nCould not find directory: " + input );
+			}
+		}
+		
+		public void setPipeline(String path) {
+			if (environment.equals( DOCKER )) {
+				File inDocker = new File(path);
+				File onSystem = new File(Config.replaceEnvVar(RunMock.TMP_PROJ), inDocker.getName() );
+				path = onSystem.getAbsolutePath();
+			}
+			this.pipeline = path;
+		}
+		
 		public String toString(){
 			ArrayList<String> row = new ArrayList<String>();
 			for (String s: outputHeader) {
@@ -69,7 +103,7 @@ public class RunMock
 	}
 
 	protected static ArrayList<String> outputHeader = new ArrayList<String>( Arrays.asList(
-			CONFIG_FILE_COL, PIPELINE_COL, NUM_COMPLETE_MODS_COL,
+			CONFIG_FILE_COL, FLAGS_COL, ENV_COL, PIPELINE_COL, NUM_COMPLETE_MODS_COL,
 			EXPECTED_OUTCOME_COL, OUTCOME_SEEN_COL, PASS_FAIL, NOTES_COL) );
 	
 	private static ArrayList<TestInfoRow> tests = new ArrayList<TestInfoRow>();
@@ -122,11 +156,13 @@ public class RunMock
 		return (new File(inF.getParentFile(), name)).getAbsolutePath();
 	}
 	
-	protected static void readTestList( String filePath ) throws FileNotFoundException, IOException
+	protected static void readTestList( String filePath ) throws Exception
 	{
 		System.err.println("Reading test list from: " + filePath);
 		File inFile = new File( filePath );
 		int iConfig = 0;
+		int iFlags = 0;
+		int iEnv = 0;
 		int iExpected = 0;
 		int iNotes = 0;
 		BufferedReader reader = BioLockJUtil.getFileReader( inFile );
@@ -140,13 +176,18 @@ public class RunMock
 				
 				if( iNotes==0 ){
 					iConfig = row.indexOf( CONFIG_FILE_COL );
+					iFlags = row.indexOf( FLAGS_COL );
+					iEnv = row.indexOf( ENV_COL );
 					iExpected = row.indexOf( EXPECTED_OUTCOME_COL );
 					iNotes = row.indexOf( NOTES_COL );
 				}else {
 					TestInfoRow oneTest = new TestInfoRow();
 					oneTest.config = new File( Config.replaceEnvVar( row.get( iConfig ) ) );
+					if (iFlags > 0 ) oneTest.flags = row.get( iFlags ); else oneTest.flags = "";
+					if (iEnv > 0 ) oneTest.environment = row.get( iEnv ); else oneTest.environment = "";
 					oneTest.out_exp = row.get( iExpected );
-					oneTest.notes = row.get( iNotes );
+					if (iNotes > 0 ) oneTest.notes = row.get( iNotes ); else oneTest.notes = "";
+					if (oneTest.environment.equals( DOCKER )) oneTest.extractInputDir();
 					tests.add( oneTest );
 				}
 			}
@@ -159,10 +200,37 @@ public class RunMock
 	}
 	
 	protected static void runMockBljMain (TestInfoRow test) throws Exception {
-		String cmd = "java -cp " + Config.replaceEnvVar(MOCK_JAR) + ":" + Config.replaceEnvVar(BLJ_JAR) 
-		+ " sheepdog.MockMain " + "-b " + Config.replaceEnvVar(TMP_PROJ) 
-		+ " -u ~ -c " + test.config.getAbsolutePath();
-		System.err.println(cmd);
+		String cmd;
+		if (test.environment.equals( DOCKER ))
+		{
+			cmd = "docker run --rm "
+					+ "-v /var/run/docker.sock:/var/run/docker.sock "
+					+ "-v " + Config.replaceEnvVar("~") + ":/home/ec2-user "
+					+ "-v " + Config.replaceEnvVar(TMP_PROJ) + ":/mnt/efs/pipelines:delegated "
+					+ "-v " + test.inputDir + ":/mnt/efs/input:ro "
+					+ "-v " + test.config.getParent() + ":/mnt/efs/config:ro "
+					+ "-v " + Config.replaceEnvVar(MOCK_DIST) + ":/modules "
+					+ "-v " + Config.replaceEnvVar(BLJ_JAR) + ":/app/biolockj/dist/BioLockJ.jar "
+					+ "biolockj/biolockj_controller java -cp /modules/*:/app/biolockj/dist/BioLockJ.jar "
+					+ "sheepdog.MockMain -u " + Config.replaceEnvVar("~") + " -b " + Config.replaceEnvVar(TMP_PROJ)
+					+ " -i " + test.inputDir + " -c " + test.config ;
+			String msg = String.valueOf( cmd );
+			// The msg printed to the console can be copy/pasted to run the command
+			// AND it has logical line breaks to make it more human readable.
+			msg = msg.replaceAll( "run --rm",  "holdThisDash"); 
+			msg = msg.replaceAll( "ec2-user",  "holdUser" );
+			msg = msg.replaceAll( "-",  " \\\\" + System.lineSeparator() + "-");
+			msg = msg.replaceAll( "biolockj/biolockj_controller",  " \\\\" + System.lineSeparator() + "biolockj/biolockj_controller");
+			msg = msg.replaceAll( "holdThisDash",  "run --rm");
+			msg = msg.replaceAll( "holdUser",  "ec2-user");
+			System.err.println(msg);
+		}else {
+			cmd = "java -cp " + Config.replaceEnvVar(MOCK_JAR) + ":" + Config.replaceEnvVar(BLJ_JAR) 
+			+ " sheepdog.MockMain " + "-b " + Config.replaceEnvVar(TMP_PROJ) 
+			+ " -u ~ -c " + test.config.getAbsolutePath()+ " " + test.flags ;
+			System.err.println(cmd);
+		}
+
 		String result = "";
 		String pipeline = "NA";
 		try {
@@ -193,9 +261,9 @@ public class RunMock
 			ex.printStackTrace();
 		}
 		test.result = result ;
-		test.pipeline = pipeline ;
+		test.setPipeline(pipeline) ;
 	}
-	
+
 	protected static void writeHeader( BufferedWriter writer ) throws IOException{
 		
 	}
---
+++ test/module/kraken2/runThisTestSet.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+${SHEP}/MockMain/resources/runMockMain.sh ${SHEP}/test/module/kraken2/Kraken2TestList.txt
