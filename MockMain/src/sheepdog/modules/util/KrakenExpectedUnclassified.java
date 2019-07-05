8237b28f27f27d790f4da942a1ca86abcafd3eef
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-07-05 12:50:09 -0400

    towards unclassified list

--- MockMain/src/sheepdog/modules/util/KrakenExpectedUnclassified.java
+++ MockMain/src/sheepdog/modules/util/KrakenExpectedUnclassified.java
@@ -1,7 +1,11 @@
 package sheepdog.modules.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class KrakenExpectedUnclassified
 {
@@ -14,11 +18,45 @@ public class KrakenExpectedUnclassified
 	public static HashMap<String, Long> getUnclassifiedMap( File inFile ) throws Exception
 	{
 		HashMap<String, Long> map = new HashMap<>();
+		List<Holder> fileLines = getFileLines(inFile);
+		
+		for( int x=0; x < fileLines.size(); x++)
+		{
+		}
 		
 		
 		
 		return map;
 	}
+	
+	private static class Holder
+	{
+		String taxaLine;
+		long taxaCount;
+	}
+	
+	private static List<Holder> getFileLines( File inFile ) throws Exception
+	{
+		List<Holder> list = new ArrayList<>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(inFile));
+		
+		for(String s= reader.readLine(); s != null; s = reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			if( splits.length != 2)
+				throw new Exception("Expecting two tokens " + inFile.getAbsolutePath() +  " "+  s);
+			
+			Holder h= new Holder();
+			h.taxaLine= splits[0];
+			h.taxaCount = Long.parseLong(splits[1]);
+			list.add(h);
+		}
+		
+		return list;
+	}
+	
 	
 	/*
 	 * 
