b44d77d8ff935b3c48a1b6784c30c2a36ee038a7
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-07-08 13:51:56 -0400

    stub for unclassified check...

--- MockMain/src/sheepdog/modules/util/RdpExpectedUnclassified.java
+++ MockMain/src/sheepdog/modules/util/RdpExpectedUnclassified.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+import parsers.NewRDPNode;
 import parsers.NewRDPParserFileLine;
+import sheepdog.modules.RdpReparse;
 
 
 public class RdpExpectedUnclassified
@@ -20,11 +23,11 @@ public class RdpExpectedUnclassified
 	}
 	
 	private static String[] TAXA_LEVELS = { "phylum" , "class" , "order" , "family" , "genus" } ;
+	private static String[] TAXA_LEVELS_FIRST_UPPER = { "Phylum" , "Class" , "Order" , "Family" , "Genus" } ;
+	
 	/*
 	 * the key will be a full string such as 
 	 * phylum__Acidobacteria|class__Acidobacteria_Gp1|order__Unclassified Acidobacteria_Gp1 Class|family__Unclassified Acidobacteria_Gp1 Class|genus__Unclassified Acidobacteria_Gp1 Class
-	 * 
-	 * 
 	 */
 	public static HashMap<String, Long> getUnclassifiedMap( File inFile , String startLevel, String endLevel ) throws Exception
 	{
@@ -39,13 +42,59 @@ public class RdpExpectedUnclassified
 			boolean keepGoing = true;
 			for( int x=startIndex; keepGoing &&  x<=endIndex; x++)
 			{
+				NewRDPNode node = rdp.getTaxaMap().get(TAXA_LEVELS[x]);
 				
+				if( node.getScore() <= RdpReparse.RDP_THRESHOLD )
+				{
+					keepGoing = false;
+					String unclassifiedString = buildUnclassifiedString(rdp, x);
+					
+					Long aLong = map.get(unclassifiedString);
+					
+					if( aLong == null)
+						aLong = 0l;
+					
+					aLong++;
+					
+					map.put(unclassifiedString, aLong);
+					
+				}
 			}
 		}
 		
 		return map;
 	}
 	
+	private static String buildUnclassifiedString(NewRDPParserFileLine rdpLine, int startUnclassifiedLevel)
+		throws Exception
+	{
+		StringBuffer buff = new StringBuffer();
+		//phylum__Acidobacteria|class__Acidobacteria_Gp1|order__Unclassified Acidobacteria_Gp1 Class|family__Unclassified Acidobacteria_Gp1 Class|genus__Unclassified Acidobacteria_Gp1 Class
+		
+		Map<String, NewRDPNode> taxaMap = rdpLine.getTaxaMap();
+		
+		for( int x=0; x < TAXA_LEVELS.length; x++ )
+		{
+			buff.append(TAXA_LEVELS[x] + "__");
+			
+			if( x < startUnclassifiedLevel)
+			{
+				String taxaName = taxaMap.get(TAXA_LEVELS[x]).getTaxaName();
+				buff.append(taxaName);
+			}
+			else
+			{
+				buff.append("Unclassified " + taxaMap.get(TAXA_LEVELS[startUnclassifiedLevel]).getTaxaName() + " "+
+									TAXA_LEVELS_FIRST_UPPER[startUnclassifiedLevel]);
+			}
+			
+			if( x + 1 < TAXA_LEVELS.length)
+				buff.append("|");
+		}
+		
+		
+		return buff.toString();
+	}
 		
 	/*
 	 * 
