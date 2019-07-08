1f36df489503407f3771726e392a64497ad361c1
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-07-08 13:29:39 -0400

    stub for expected RDP

--- MockMain/src/sheepdog/modules/util/RdpExpectedUnclassified.java
+++ MockMain/src/sheepdog/modules/util/RdpExpectedUnclassified.java
@@ -1,17 +1,11 @@
 package sheepdog.modules.util;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
 
-import parsers.NewRDPNode;
 import parsers.NewRDPParserFileLine;
-import sheepdog.modules.RdpReparse;
 
 
 public class RdpExpectedUnclassified
@@ -26,11 +20,11 @@ public class RdpExpectedUnclassified
 	}
 	
 	private static String[] TAXA_LEVELS = { "phylum" , "class" , "order" , "family" , "genus" } ;
-	private static String[] TAXA_LEVELS_FIRST_UPPER = { "Phylum" , "Class" , "Order" , "Family" , "Genus" } ;
-	
 	/*
 	 * the key will be a full string such as 
 	 * phylum__Acidobacteria|class__Acidobacteria_Gp1|order__Unclassified Acidobacteria_Gp1 Class|family__Unclassified Acidobacteria_Gp1 Class|genus__Unclassified Acidobacteria_Gp1 Class
+	 * 
+	 * 
 	 */
 	public static HashMap<String, Long> getUnclassifiedMap( File inFile , String startLevel, String endLevel ) throws Exception
 	{
@@ -45,106 +39,14 @@ public class RdpExpectedUnclassified
 			boolean keepGoing = true;
 			for( int x=startIndex; keepGoing &&  x<=endIndex; x++)
 			{
-				NewRDPNode node = rdp.getTaxaMap().get(TAXA_LEVELS[x]);
 				
-				if( node.getScore() <= RdpReparse.RDP_THRESHOLD )
-				{
-					keepGoing = false;
-					String unclassifiedString = buildUnclassifiedString(rdp, x);
-					
-					Long aLong = map.get(unclassifiedString);
-					
-					if( aLong == null)
-						aLong = 0l;
-					
-					aLong++;
-					
-					map.put(unclassifiedString, aLong);
-					
-				}
 			}
 		}
 		
 		return map;
 	}
 	
-	private static String buildUnclassifiedString(NewRDPParserFileLine rdpLine, int startUnclassifiedLevel)
-		throws Exception
-	{
-		StringBuffer buff = new StringBuffer();
-		//phylum__Acidobacteria|class__Acidobacteria_Gp1|order__Unclassified Acidobacteria_Gp1 Class|family__Unclassified Acidobacteria_Gp1 Class|genus__Unclassified Acidobacteria_Gp1 Class
 		
-		Map<String, NewRDPNode> taxaMap = rdpLine.getTaxaMap();
-		
-		for( int x=0; x < TAXA_LEVELS.length; x++ )
-		{
-			buff.append(TAXA_LEVELS[x] + "__");
-			
-			if( x < startUnclassifiedLevel)
-			{
-				String taxaName = taxaMap.get(TAXA_LEVELS[x]).getTaxaName();
-				buff.append(taxaName);
-			}
-			else
-			{
-				buff.append("Unclassified " + taxaMap.get(TAXA_LEVELS[startUnclassifiedLevel]).getTaxaName() + " "+
-									TAXA_LEVELS_FIRST_UPPER[startUnclassifiedLevel]);
-			}
-			
-			if( x + 1 < TAXA_LEVELS.length)
-				buff.append("|");
-		}
-		
-		
-		return buff.toString();
-	}
-	
-	private static String getLastPipeSubstring(String s)
-	{
-		s = s.substring(s.lastIndexOf("|") + 1, s.length());
-		return s;
-	}
-	
-	public static void assertUnclassifiedEquals(File biolockJFile, HashMap<String, Long> map) throws Exception
-	{
-		BufferedReader reader = new BufferedReader(new FileReader(biolockJFile));
-		
-		for(String s= reader.readLine(); s != null; s= reader.readLine())
-		{
-			StringTokenizer sToken = new StringTokenizer(s, "\t");
-			
-			if( sToken.countTokens() != 2)
-				throw new Exception("Expecting two tokens " );
-			
-			String taxa =sToken.nextToken();
-			Long count = Long.parseLong(sToken.nextToken());
-			
-			if( getLastPipeSubstring(taxa).toLowerCase().indexOf("unclassified")!=-1)
-			{
-
-				Long anotherCount = map.get(taxa);
-				
-				if( anotherCount == null)
-				{
-					//if( taxa.indexOf("Verrucomicrobia") != -1)
-					System.out.println("Could not find " + taxa + " " + count);
-					
-				}
-				else
-				{
-
-					if( ! count.equals(anotherCount))
-						System.out.println("Taxa MISMATCH " + taxa +  " " + count + " " + anotherCount);
-					else
-					//if( taxa.indexOf("Verrucomicrobia") != -1)	
-					System.out.println("match " + taxa + " " + count);
-				}
-					
-			}
-			
-		}
-	}
-	
 	/*
 	 * 
 	 * Hard-coded file path for the development cycle here.
@@ -158,12 +60,7 @@ public class RdpExpectedUnclassified
 		
 		for(String s : map.keySet())
 		{
-			if( s.indexOf("Gp1") != -1 )
-				System.out.println(s + " " +  map.get(s) );
+			System.out.println(s + " " +  map.get(s) + "\n");
 		}
-		
-		File matchingFile = new File("C:\\sheepDog\\sheepdog_testing_suite\\MockMain\\pipelines\\verifyRDPParser_2019Jul08\\03_RdpParser\\output\\verifyRDPParser_2019Jul08_otuCount_ERR1456828.tsv");
-		
-		assertUnclassifiedEquals(matchingFile,map);
 	}
 }
