47ca1a3be09fca6739f300e69c25b139211a3ca6
Anthony Fodor <anthony.fodor@gmail.com>
2019-07-25 21:16:41 -0400

    stub for Check Count table

--- MockMain/src/sheepdog/modules/CheckCountTable.java
+++ MockMain/src/sheepdog/modules/CheckCountTable.java
@@ -1,6 +1,32 @@
 package sheepdog.modules;
 
-public class CheckCountTable
-{
+import java.io.File;
+import java.util.List;
 
+import biolockj.Log;
+import biolockj.module.BioModuleImpl;
+import biolockj.module.classifier.ClassifierModule;
+import biolockj.util.ModuleUtil;
+
+public class CheckCountTable extends BioModuleImpl
+{
+	@Override
+	public void checkDependencies() throws Exception
+	{
+		Log.info( getClass(), "IN stub for checkDependencies()");
+		
+	}
+	
+	@Override
+	public void executeTask() throws Exception
+	{
+		Log.info( getClass(), "IN stub for executeTask()");
+		
+		ClassifierModule cm =  ModuleUtil.getClassifier(this, false);
+		
+		Log.info( getClass(), "module output directory = " +  cm.getOutputDir().getAbsolutePath());
+		
+		List<File> inputFiles = getInputFiles();
+		
+	}
 }
