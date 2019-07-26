f7a6abf3675a9ee4e0012c9a746019244893e3a3
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-07-26 15:09:31 -0400

    throws null pointer exception

--- MockMain/src/sheepdog/modules/CheckCountTable.java
+++ MockMain/src/sheepdog/modules/CheckCountTable.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.List;
 
 import biolockj.Log;
+import biolockj.module.BioModule;
 import biolockj.module.BioModuleImpl;
 import biolockj.module.classifier.ClassifierModule;
 import biolockj.util.ModuleUtil;
@@ -18,15 +19,24 @@ public class CheckCountTable extends BioModuleImpl
 	}
 	
 	@Override
+	public boolean isValidInputModule(BioModule module)
+	{
+		return false;
+	}
+	
+	@Override
 	public void executeTask() throws Exception
 	{
 		Log.info( getClass(), "IN stub for executeTask()");
 		
 		ClassifierModule cm =  ModuleUtil.getClassifier(this, false);
 		
-		Log.info( getClass(), "module output directory = " +  cm.getOutputDir().getAbsolutePath());
+		Log.info( getClass(), " TEST_ME module output directory = " +  cm.getOutputDir().getAbsolutePath());
 		
 		List<File> inputFiles = getInputFiles();
+		
+		for( File f : inputFiles)
+			Log.info( getClass(), " TEST_ME input file = " +  f.getAbsolutePath());
 		
 	}
 }
