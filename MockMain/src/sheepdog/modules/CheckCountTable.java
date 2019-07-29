2e03a29d273d28d01528e330e7e5ed66ff7a7024
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-07-29 12:08:08 -0400

    check count passing in BiolockJ...

--- MockMain/src/sheepdog/modules/CheckCountTable.java
+++ MockMain/src/sheepdog/modules/CheckCountTable.java
@@ -1,14 +1,15 @@
 package sheepdog.modules;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import biolockj.Log;
 import biolockj.module.BioModule;
 import biolockj.module.BioModuleImpl;
-import biolockj.module.implicit.parser.wgs.Kraken2Parser;
 import biolockj.module.report.taxa.BuildTaxaTables;
 import biolockj.util.ModuleUtil;
+import sheepdog.modules.util.CompareCountTable;
 
 public class CheckCountTable extends BioModuleImpl
 {
@@ -16,7 +17,6 @@ public class CheckCountTable extends BioModuleImpl
 	public void checkDependencies() throws Exception
 	{
 		Log.info( getClass(), "IN stub for checkDependencies()");
-		
 	}
 	
 	@Override
@@ -25,12 +25,37 @@ public class CheckCountTable extends BioModuleImpl
 		return false;
 	}
 	
+	private static File findExactlyOne(String level, List<File> list) throws Exception
+	{
+		File aFile = null;
+		
+		for(File f : list)
+		{
+			if( f.getName().indexOf(level) != - 1)
+			{
+				if( aFile != null)
+					throw new Exception("Found duplicate for " + level);
+				
+				aFile = f;
+			}
+		}
+		
+		if( aFile == null)
+			throw new Exception("Could not find " + level);
+		
+		return aFile;
+	}
+	
 	@Override
 	public void executeTask() throws Exception
 	{
 		Log.info( getClass(), "IN stub for executeTask()");
 		
 		List<BioModule> mods =  ModuleUtil.getModules(this, false);
+		
+		List<File> countTables = new ArrayList<>();
+		
+		List<File> inputFiles =null;
 		
 		for( BioModule bm : mods)
 		{
@@ -48,14 +73,43 @@ public class CheckCountTable extends BioModuleImpl
 
 					Log.info( getClass(), " TEST_ME module output File = " +  s+ " " + 
 							bm.getOutputDir().getAbsolutePath());
+					countTables.add(new File( outputDir.getAbsolutePath() + File.separator + s ));
 				}
+				
+				inputFiles= bm.getInputFiles();
 			}
 		}
 		
-		List<File> inputFiles = getInputFiles();
+		File parentDir = null;
 		
 		for( File f : inputFiles)
+		{
 			Log.info( getClass(), " TEST_ME input file = " +  f.getAbsolutePath());
+			
+			if(parentDir == null)
+			{
+				parentDir = f.getParentFile();
+			}
+			else
+			{
+				if( ! parentDir.equals(f.getParentFile()))
+					throw new Exception("Inconsistent parent directory " + parentDir + " " + f.getParentFile());
+			}
+		}
+			
+		
+		for(String level : CompareCountTable.LEVELS)
+		{
+			File countTable = findExactlyOne(level, countTables);
+			
+			Log.info( getClass(), " TEST_ME check  " + countTable.getAbsolutePath());
+			CompareCountTable.assertEquals(parentDir, countTable, level);
+
+			Log.info( getClass(), " TEST_ME pass " + countTable.getAbsolutePath());
+		}
+		
+
+		Log.info( getClass(), "Global pass ");
 		
 	}
 }
