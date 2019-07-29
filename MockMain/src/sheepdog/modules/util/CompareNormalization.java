7616f281ad41e10dcca775691d781fd72ad5f7e6
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-07-29 13:13:32 -0400

    passign with manually imported file names

--- MockMain/src/sheepdog/modules/util/CompareNormalization.java
+++ MockMain/src/sheepdog/modules/util/CompareNormalization.java
@@ -9,6 +9,31 @@ public class CompareNormalization
 		OtuWrapper unnormWrapper = new OtuWrapper(unNormalizedFile);
 		
 		OtuWrapper normWrapper = new OtuWrapper(normalizedFile);
+		
+		if(unnormWrapper.getOtuNames().size() != normWrapper.getOtuNames().size())
+			throw new Exception("Mismatch on number of taxa " +unnormWrapper.getOtuNames().size() + " " 
+						+ normWrapper.getOtuNames().size());
+		
+		if(unnormWrapper.getSampleNames().size() != normWrapper.getSampleNames().size())
+			throw new Exception("Mismatch on sample size");
+		
+		for( int x=0; x < unnormWrapper.getSampleNames().size(); x++)
+		{
+			String sampleName = unnormWrapper.getSampleNames().get(x);
+			
+			for( int y=0; y < unnormWrapper.getOtuNames().size(); y++)
+			{
+				String taxaName = unnormWrapper.getOtuNames().get(y);
+				
+				double expectedVal = unnormWrapper.getDataPointsNormalizedThenLogged().get(x).get(y);
+				
+				double anotherVal = normWrapper.getDataPointsUnnormalized().get(normWrapper.getIndexForSampleName(sampleName))
+						.get(normWrapper.getIndexForOtuName(taxaName));
+				
+				if( Math.abs(  expectedVal - anotherVal ) > 0.0001)
+					throw new Exception("Mismatch " + sampleName + " " + taxaName + " " + expectedVal + " " + anotherVal);
+			}
+		}
 	}
 	
 	public static void main(String[] args) throws Exception
@@ -27,5 +52,7 @@ public class CompareNormalization
 			assertEquals(unNorm, norm);
 			
 		}
+		
+		System.out.println("Global pass");
 	}
 }
