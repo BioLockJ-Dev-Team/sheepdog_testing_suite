b3f27abd8e87c1c2eccd847a0c483a2e3c3fae2a
Ivory <IvoryEC@gmail.com>
2019-09-12 13:40:36 -0400

    Create the release-testing test collections for each environment.

---
+++ test/run_docker_testSets.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Docker test collection
+
+# Each 'run this test set' script prints output to the screen
+# Each line pipes that output to tee so it is
+# printed for you to see in real-time AND saved to a file.
+# At the end, this script a cliff-notes summary.
+
+RES=${SHEP}/test/results_docker_testSet_NOT_IN_GIT.txt
+date > $RES
+
+# module tests
+DIR=${SHEP}/test/module
+${DIR}/assembly/docker_runThisTestSet.sh        2>&1 | tee -a $RES
+${DIR}/kraken2/docker_runThisTestSet.sh         2>&1 | tee -a $RES
+
+# full pipeline
+${SHEP}/test/docker/docker_runThisTestSet.sh    2>&1 | tee -a $RES
+
+
+# print the quick-reference version of the results
+echo "* * * * * * * * * * * * * * * * * * * *"
+echo "Summary from $RES:"
+echo ""
+grep "Test results\|Number of tests that" $RES
