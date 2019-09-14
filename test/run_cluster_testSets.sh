30cf888697fddb3c77e6a4a23631ab47c1ee5ab5
Ivory <IvoryEC@gmail.com>
2019-09-13 21:21:20 -0400

    Add the cluster-specific assembly test to the cluster test collection.

--- test/run_cluster_testSets.sh
+++ test/run_cluster_testSets.sh
@@ -3,35 +3,31 @@
 
 # Local test collection
 
-# Each 'run this test set' script prints output to the screen
+# Each 'run this test set' script prints output to the screen.
 # Each line pipes that output to tee so it is
 # printed for you to see in real-time AND saved to a file.
-# At the end, this script a cliff-notes summary.
+# At the end, this script prints a cliff-notes summary.
 
-RES=${SHEP}/test/results_local_testSet_NOT_IN_GIT.txt
+RES=${SHEP}/test/results_cluster_testSet_NOT_IN_GIT.txt
 date > $RES
 start=$(date '+%s')
 
+${SHEP}/MockMain/resources/runMockMain.sh ${SHEP}/test/module/assembly/cluster
+_testList.txt      2>&1 | tee -a $RES
 
-# biolockj ${SHEP}/test/module/assembly/test_assembly_cluster.properties
+# In theory, any local pipeline can run on the cluster as long 
+# as the file paths are provided correctly in local.properties.
 
-# In theory, any local pipeline can run on the cluster as long as the file paths are provided correctly in local.properties.
-
-
-# module tests
+# local module tests
 DIR=${SHEP}/test/module
-#${DIR}/assembly/runThisTestSet.sh              2>&1 | tee -a $RES
-${DIR}/email/runThisTestSet.sh                 2>&1 | tee -a $RES
+${DIR}/assembly/runThisTestSet.sh              2>&1 | tee -a $RES
+#${DIR}/email/runThisTestSet.sh                 2>&1 | tee -a $RES
 ${DIR}/rdp/runThisTestSet.sh                   2>&1 | tee -a $RES
 #${DIR}/rdpParser/runThisTestSet.sh             2>&1 | tee -a $RES
 ${DIR}/validationUtil/runThisTestSet.sh        2>&1 | tee -a $RES
 #${DIR}/kraken2/runThisTestSet.sh               2>&1 | tee -a $RES
 #${DIR}/kraken2Parser/runThisTestSet.sh         2>&1 | tee -a $RES
 ${DIR}/GenMod/runThisTestSet.sh                2>&1 | tee -a $RES
-
-# the sheepdog quickstart example
-DIR=${SHEP}/MockMain/resources
-${DIR}/runThisTestSet.sh                       2>&1 | tee -a $RES
 
 # full pipeline
 ${SHEP}/test/local/runThisTestSet.sh           2>&1 | tee -a $RES
