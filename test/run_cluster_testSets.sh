2ee425fa8f60e90527c273890f3a92638e40c51c
Ivory <IvoryEC@gmail.com>
2019-09-13 22:20:43 -0400

    Edit cluster test collection

--- test/run_cluster_testSets.sh
+++ test/run_cluster_testSets.sh
@@ -12,15 +12,14 @@ RES=${SHEP}/test/results_cluster_testSet_NOT_IN_GIT.txt
 date > $RES
 start=$(date '+%s')
 
-${SHEP}/MockMain/resources/runMockMain.sh ${SHEP}/test/module/assembly/cluster
-_testList.txt      2>&1 | tee -a $RES
+# ${SHEP}/MockMain/resources/runMockMain.sh ${SHEP}/test/module/assembly/cluster_testList.txt      2>&1 | tee -a $RES
 
 # In theory, any local pipeline can run on the cluster as long 
 # as the file paths are provided correctly in local.properties.
 
 # local module tests
 DIR=${SHEP}/test/module
-${DIR}/assembly/runThisTestSet.sh              2>&1 | tee -a $RES
+#${DIR}/assembly/runThisTestSet.sh              2>&1 | tee -a $RES
 #${DIR}/email/runThisTestSet.sh                 2>&1 | tee -a $RES
 ${DIR}/rdp/runThisTestSet.sh                   2>&1 | tee -a $RES
 #${DIR}/rdpParser/runThisTestSet.sh             2>&1 | tee -a $RES
@@ -29,7 +28,7 @@ ${DIR}/validationUtil/runThisTestSet.sh        2>&1 | tee -a $RES
 #${DIR}/kraken2Parser/runThisTestSet.sh         2>&1 | tee -a $RES
 ${DIR}/GenMod/runThisTestSet.sh                2>&1 | tee -a $RES
 
-# full pipeline
+# full local pipeline
 ${SHEP}/test/local/runThisTestSet.sh           2>&1 | tee -a $RES
 
 end=$(date '+%s')
