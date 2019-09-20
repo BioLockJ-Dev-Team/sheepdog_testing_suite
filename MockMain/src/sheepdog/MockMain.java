fc4d7f5ce597339362f2d1fd69de677ccfc5355a
Ivory <IvoryEC@gmail.com>
2019-09-20 15:14:43 -0400

    Use real BioLockJ (not MockMain) to assert that validation is enabled.

--- MockMain/src/sheepdog/MockMain.java
+++ MockMain/src/sheepdog/MockMain.java
@@ -2,11 +2,7 @@ package sheepdog;
 
 import biolockj.Config;
 import biolockj.Constants;
-import biolockj.exception.ConfigFormatException;
-import biolockj.exception.ConfigPathException;
 import biolockj.exception.FatalExceptionHandler;
-import biolockj.module.BioModule;
-import biolockj.module.BioModuleImpl;
 import biolockj.util.BioLockJUtil;
 
 public class MockMain extends biolockj.BioLockJ
@@ -22,7 +18,6 @@ public class MockMain extends biolockj.BioLockJ
 		try {
 			initBioLockJ( args );
 			System.out.println( PIPELINE_KEY + Config.getPipelineDir().getAbsolutePath() );
-			checkValidation();
 			runPipeline();
 			result = COMPLETE;
 		} catch( final Exception ex ) {
@@ -32,22 +27,6 @@ public class MockMain extends biolockj.BioLockJ
 			System.out.println( RESULT_KEY + result );
 			if( !BioLockJUtil.isDirectMode() ) pipelineShutDown();
 		}
-	}
-	
-	public static void checkValidation() throws ConfigFormatException, ConfigPathException {
-		BioModule module = new BioModuleImpl() {
-			@Override
-			public void executeTask() throws Exception {}
-			@Override
-			public void checkDependencies() throws Exception {}
-		};
-		
-		if (Config.getBoolean( module, "validation.stopPipeline" )
-						&& !Config.getBoolean( module, "validation.disableValidation" )
-						&& Config.getString( module, "validation.expectationFile" ) != null ) {
-			System.out.println(VALIDATION_ENABLED);
-		}
-		System.out.println(VALIDATION_ENABLED + "-->NOPE");
 	}
 	
 }
