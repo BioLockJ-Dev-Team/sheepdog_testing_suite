fcd64d43e8d64354287dcd4d5d2c51cc79362f88
Ivory <IvoryEC@gmail.com>
2019-10-01 16:41:46 -0400

    Create and use the generalize.sh script  * the generalize.sh script strips out computer-specific paths and todays DATE from a text file  * testCommandLine.sh check_it method now evaluates some outputs in their 'generic' form, indicated by 'check_it g'  * no more expected_examples; no need to manually determine if the computer-specific output was correct.

---
+++ test/bash/generalize.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+INPUT_NAME=$1
+OUTPUT_NAME=${INPUT_NAME%.out}_generic.out
+
+DATE=$(date '+%Y%b%d')
+
+while read -r line; do
+	# replace SHEP
+	line=${line//"${SHEP}"/"<SHEP>"}
+	# replace BLJ_PROJ
+	line=${line//"${BLJ_PROJ}"/"<BLJ_PROJ>"}
+	# replace BLJ
+	line=${line//"$BLJ"/"<BLJ>"}
+	# replace todays date
+	line=${line//"${DATE}"/"<DATE>"}
+	# write to output
+	echo $line >> $OUTPUT_NAME
+done < $1
+
+echo $OUTPUT_NAME
