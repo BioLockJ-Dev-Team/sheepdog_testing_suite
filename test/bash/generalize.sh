2d2c99fd4bc74fd78ce8b8abbfca1b004f6fda64
Ivory <IvoryEC@gmail.com>
2019-10-23 09:42:05 -0400

    Generalize the version from bash output.

--- test/bash/generalize.sh
+++ test/bash/generalize.sh
@@ -4,6 +4,7 @@ INPUT_NAME=$1
 OUTPUT_NAME=${INPUT_NAME%.out}_generic.out
 
 DATE=$(date '+%Y%b%d')
+VERSION=$(cat ${SHEP}/biolockjVersion)
 
 while read -r line; do
 	# replace SHEP
@@ -14,8 +15,10 @@ while read -r line; do
 	line=${line//"$BLJ"/"<BLJ>"}
 	# replace todays date
 	line=${line//"${DATE}"/"<DATE>"}
+	# replace current version
+	line=${line//"${VERSION}"/"<VERSION>"}
 	# write to output
-	echo $line >> $OUTPUT_NAME
+	echo $line >> $OUTPUT_NAME	
 done < $1
 
 echo $OUTPUT_NAME
