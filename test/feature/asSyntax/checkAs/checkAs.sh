95c7b90013d44e0baf9a5eada20170948de3a8f9
Ivory <IvoryEC@gmail.com>
2019-12-16 11:50:50 -0500

    Test set for the AS syntax feature.

---
+++ test/feature/asSyntax/checkAs/checkAs.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+OUT="../output/showThatAsWorked.txt"
+
+ls ../../0* > $OUT
+
+#echo "" >> $OUT
+#echo "summary.txt:" >> $OUT
+#egrep "SeqFileValidator|Sfv|RarefySeqs|Rseqs|GenMod|Bash" ../../summary.txt >> $OUT
+
+#echo "" >> $OUT
+#echo "Used properties:" >> $OUT
+#egrep "SeqFileValidator|Sfv|RarefySeqs|Rseqs|GenMod|Bash" ../../FINAL_*.properties >> $OUT
