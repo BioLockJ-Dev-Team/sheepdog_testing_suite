90e2acebf8c5571743f6babb08a3d2acbbcbfea1
Ivory <IvoryEC@gmail.com>
2020-01-17 16:01:38 -0500

    ...api tests... still not ripe.

--- test/bash/testAPI.sh
+++ test/bash/testAPI.sh
@@ -1,15 +1,21 @@
 # Test the biolockj API
 
 MOD=biolockj.module.report.taxa.AddPseudoCount
-#MORE_MODS=/path/to/dir/with/MODS
+MORE_MODS=$SHEP/MockMain/dist
 
 # get a list of all modules
 biolockj_api listModules 2> /dev/null
 
+# list all modules, including extra modules in $MORE_MODS
+biolockj_api listModules $MORE_MODS
+
 # get a list of modules that use the API_Module interface
 biolockj_api listApiModules 2> /dev/null
 
-# for each module that uses the interface, get a list of properties
+# get ApiModules including extra modules
+biolockj_api listApiModules $MORE_MODS 2> /dev/null
+
+# for a module that uses the interface, get a list of properties
 biolockj_api listProps $MOD
 
 MODS=$(biolockj_api listApiModules 2> /dev/null)
@@ -25,25 +31,26 @@ biolockj_api listProps
 biolockj_api listAllProps  2> /dev/null
 
 # Get all props including props for extra modules
-#biolockj_api listAllProps $MORE_MODS
-
-# get the description for a few properties from a module
-biolockj_api describeProp pipeline.defaultSeqMerger $MOD # null
-biolockj_api describeProp script.defaultHeader $MOD # text
-
-# get the description for a few properties from the backbone
-biolockj_api describeProp pipeline.defaultSeqMerger
-biolockj_api describeProp pipeline.detachJavaModules
+biolockj_api listAllProps $MORE_MODS 2> /dev/null
 
 # get the format type for a few properties for an individual module
-biolockj_api propType pipeline.detachJavaModules $MOD # Boolean
-biolockj_api propType pipeline.defaultStatsModule $MOD # String
+biolockj_api propType script.numThreads $MOD # integer
+biolockj_api propType cluster.batchCommand $MOD # String
+biolockj_api propType pipeline.defaultStatsModule $MOD # null for the module
 biolockj_api propType pipeleeene.Typo $MOD # null
 
 # get the format type for a few properties for the backbone
 biolockj_api propType pipeline.detachJavaModules # Boolean
 biolockj_api propType pipeline.defaultStatsModule # String
 biolockj_api propType pipeleeene.Typo # null
+
+# get the description for a few properties from a module
+biolockj_api describeProp pipeline.defaultSeqMerger $MOD # null
+biolockj_api describeProp script.defaultHeader $MOD # text
+
+# get the description for a few properties from the backbone
+biolockj_api describeProp pipeline.defaultSeqMerger # text
+biolockj_api describeProp pipeline.detachJavaModules # text
 
 # test a prop/val pair for a module (one good, bad, null)
 biolockj_api isValidProp script.numThreads 1 $MOD # true
@@ -57,27 +64,17 @@ biolockj_api isValidProp script.numTYPO 1 $MOD # null
 # get the value for a given property, with now config file (ie, defaults)
 biolockj_api propValue script.numThreads
 
-# get a json string with lots of this data using allProperties
+# get a json string with lots of this data using the general properties
+biolockj_api propInfo
+
+# get a json string with all info on all available modules
+biolockj_api moduleInfo
+
+# get a json string with all info on all modules including extra modules
+biolockj_api moduleInfo $MORE_MODS
 
 # show help menu
 biolockj_api help
 # show help menu
 biolockj_api 
 
-# how many properties are listed in standard properties:
-grep "\." $BLJ/resources/config/default/standard.properties | wc -l
-# how many properties are listed using this API:
-biolockj_api listAllProps  2> /dev/null | wc -l
-
-
-allProps=$(biolockj_api listProps  2> /dev/null)
-for prop in $allProps; do
-	echo ""
-	printf '%-15s %-55s\n' "property:" $prop
-	type=$(biolockj_api propType $prop)
-	printf '%-15s %-55s\n' "type:" "$type"
-	desc=$(biolockj_api describeProp $prop)
-	printf '%-15s %-55s\n' "description:" "$desc"
-	default=$(biolockj_api propValue $prop)
-	printf '%-15s %-55s\n' "default:" "$default"
-done  
