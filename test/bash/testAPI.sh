179118583bb090066dc4eb2bf7f0e9f928a10be8
Ivory <IvoryEC@gmail.com>
2020-02-06 16:45:35 -0500

    Change name from biolockj_api to biolockj-api.

--- test/bash/testAPI.sh
+++ test/bash/testAPI.sh
@@ -4,80 +4,81 @@ MOD=biolockj.module.report.taxa.AddPseudoCount
 MORE_MODS=$SHEP/MockMain/dist
 
 # get a list of all modules
-biolockj_api listModules 2> /dev/null
+biolockj-api listModules 
 
 # list all modules, including extra modules in $MORE_MODS
-biolockj_api listModules $MORE_MODS
+biolockj-api listModules $MORE_MODS
 
 # get a list of modules that use the API_Module interface
-biolockj_api listApiModules 2> /dev/null
+biolockj-api listApiModules 
 
 # get ApiModules including extra modules
-biolockj_api listApiModules $MORE_MODS 2> /dev/null
+biolockj-api listApiModules $MORE_MODS 
 
 # for a module that uses the interface, get a list of properties
-biolockj_api listProps $MOD
+biolockj-api listProps $MOD
 
-MODS=$(biolockj_api listApiModules 2> /dev/null)
+MODS=$(biolockj-api listApiModules )
 for m in $MODS; do
 	echo "***  $m"
-	biolockj_api listProps $m
+	biolockj-api listProps $m
 done 
 
 # Get a list of properties for the backbone
-biolockj_api listProps
+biolockj-api listProps
 
 # Get a complete list of properties for the backbone and all modules
-biolockj_api listAllProps  2> /dev/null
+biolockj-api listAllProps 
 
 # Get all props including props for extra modules
-biolockj_api listAllProps $MORE_MODS 2> /dev/null
+biolockj-api listAllProps $MORE_MODS 
 
 # get the format type for a few properties for an individual module
-biolockj_api propType script.numThreads $MOD # integer
-biolockj_api propType cluster.batchCommand $MOD # String
-biolockj_api propType pipeline.defaultStatsModule $MOD # null for the module
-biolockj_api propType pipeleeene.Typo $MOD # null
+biolockj-api propType script.numThreads $MOD # integer
+biolockj-api propType cluster.batchCommand $MOD # String
+biolockj-api propType pipeline.defaultStatsModule $MOD # null for the module
+biolockj-api propType pipeleeene.Typo $MOD # null
 
 # get the format type for a few properties for the backbone
-biolockj_api propType pipeline.detachJavaModules # Boolean
-biolockj_api propType pipeline.defaultStatsModule # String
-biolockj_api propType pipeleeene.Typo # null
+biolockj-api propType pipeline.detachJavaModules # Boolean
+biolockj-api propType pipeline.defaultStatsModule # String
+biolockj-api propType pipeleeene.Typo # null
 
 # get the description for a few properties from a module
-biolockj_api describeProp pipeline.defaultSeqMerger $MOD # null
-biolockj_api describeProp script.defaultHeader $MOD # text
+biolockj-api describeProp pipeline.defaultSeqMerger $MOD # null
+biolockj-api describeProp script.defaultHeader $MOD # text
 
 # get the description for a few properties from the backbone
-biolockj_api describeProp pipeline.defaultSeqMerger # text
-biolockj_api describeProp pipeline.detachJavaModules # text
+biolockj-api describeProp pipeline.defaultSeqMerger # text
+biolockj-api describeProp pipeline.detachJavaModules # text
 
 # test a prop/val pair for a module (one good, bad, null)
-biolockj_api isValidProp script.numThreads 1 $MOD # true
-biolockj_api isValidProp script.numThreads foo $MOD # false
-biolockj_api isValidProp script.numTYPO 1 $MOD # null
+biolockj-api isValidProp script.numThreads 1 $MOD # true
+biolockj-api isValidProp script.numThreads foo $MOD # false
+biolockj-api isValidProp script.numTYPO 1 $MOD # null
 
 # test a prop/val pair for the backbone (one good, bad, null)
-biolockj_api isValidProp script.numThreads 1 2> /dev/null # true
-biolockj_api isValidProp script.numThreads apple 2> /dev/null 
+biolockj-api isValidProp script.numThreads 1 # true
+biolockj-api isValidProp script.numThreads apple 
 
 
 # get the value for a given property given a config file
+biolockj-api
 
-# get the value for a given property, with now config file (ie, defaults)
-biolockj_api propValue script.numThreads
+# get the value for a given property, with no config file (ie, defaults)
+biolockj-api propValue script.numThreads
 
 # get a json string with lots of this data using the general properties
-biolockj_api propInfo 2> /dev/null
+biolockj-api propInfo
 
 # get a json string with all info on all available modules
-biolockj_api moduleInfo 2> /dev/null
+biolockj-api moduleInfo
 
 # get a json string with all info on all modules including extra modules
-biolockj_api moduleInfo $MORE_MODS
+biolockj-api moduleInfo $MORE_MODS
 
 # show help menu
-biolockj_api help
+biolockj-api help
 # show help menu
-biolockj_api 
+biolockj-api 
 
