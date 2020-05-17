1d15b41ed7046162cf061003e79a2024b396f351
Anthony Fodor <anthony.fodor@gmail.com>
2020-05-17 14:26:03 -0400

    Still need to correct for round-off error...

--- MockMain/src/sheepdog/modules/CheckShannon.java
+++ MockMain/src/sheepdog/modules/CheckShannon.java
@@ -1,23 +1,24 @@
 package sheepdog.modules;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 import biolockj.Constants;
 import biolockj.Log;
 import biolockj.module.BioModuleImpl;
 import biolockj.util.BioLockJUtil;
-import sheepdog.modules.util.KrakenExpectedUnclassified;
 
 public class CheckShannon extends BioModuleImpl
 {
 	@Override
 	public void checkDependencies() throws Exception
 	{
-
-		Log.info( getClass(), "IN stub for checkDendencies()");
-		
+		Log.info( getClass(), "IN stub for checkDendencies()");	
 	}
 	
 	
@@ -32,20 +33,25 @@ public class CheckShannon extends BioModuleImpl
 		
 		for(File f : pipelineInput)
 		{
-			File matching = findMatching(f, inputFiles);
-			Log.info( getClass(), "HELLO: Compare " + f.getAbsolutePath() + " " + matching.getAbsolutePath());
+			HashMap<String, Double> orginalMap = getDiversityMap(f);
 			
-			//todo actually compre the files!
+			File matching = findMatching(f, inputFiles);
+			Log.info( getClass(), "Compare " + f.getAbsolutePath() + " " + matching.getAbsolutePath());
+			
+			HashMap<String, Double> biolockJMap = getOutputMap(matching);
+			
+			if( ! biolockJMap.equals(orginalMap))
+				throw new Exception("Mismatch " + biolockJMap + " " + orginalMap);
+			else
+				Log.info( getClass(), "Matching maps " + biolockJMap + " " + orginalMap);
 			
 			Log.info( getClass(), "HELLO:  Pass " + f.getAbsolutePath() + " " + matching.getAbsolutePath());
 			
 		}
 		
 		Log.info( getClass(), "Exit stub for executeTask() with global pass");
-	
 		
 	}
-	
 	
 
 	private static File findMatching(File pipelineFile, List<File> inputFiles) throws Exception
@@ -71,6 +77,83 @@ public class CheckShannon extends BioModuleImpl
 			throw new Exception("Could not find match for " + pipelineFile.getAbsolutePath());
 		
 		return returnFile;
+	}
+	
+	private static HashMap<String, Double> getOutputMap(File file)  throws Exception
+	{
+		
+		HashMap<String, Double> map = new HashMap<String, Double>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		reader.readLine();
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			if( splits.length != 2)
+				throw new Exception("Unexpected line " + s + " " + file.getAbsolutePath());
+			
+			String key = splits[0];
+			double val = Double.parseDouble(splits[1]);
+			
+			if( map.containsKey(key))
+				throw new Exception("Duplicate key " + key);
+			
+			map.put(key,val);
+			
+		}	
+		
+		reader.close();
+		
+		return map;
+	}
+	
+	private static HashMap<	String, Double> getDiversityMap(File file) throws Exception
+	{
+		HashMap<String, Double> map = new HashMap<String, Double>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		reader.readLine();
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			String key = splits[0];
+			
+			if( map.containsKey(key))
+				throw new Exception("Duplicate key " + file.getAbsolutePath());
+			
+			List<Double> counts = new ArrayList<Double>();
+			
+			double sum =0;
+			
+			for( int x=1; x < splits.length; x++)
+			{
+				Double aVal = Double.parseDouble(splits[x]);
+				sum += aVal;
+				counts.add(aVal);
+			}
+			
+			double shannon = 0;
+			
+			for(Double d : counts)
+			{
+				double p = d / sum;
+				
+				if( p > 0 )
+					shannon += p * Math.log(p);
+			}
+			
+			if( shannon > 0 )
+				map.put(key, -shannon);
+			else
+				map.put(key, shannon);
+		}
+		
+		reader.close();
+		return map;
 	}
 	
 	
