aa59f0abd59c89bbd99a639f7f830dd1c3643d5a
Anthony Fodor <anthony.fodor@gmail.com>
2020-05-17 12:51:19 -0400

    testing stub but finding matching files

--- MockMain/src/sheepdog/modules/CheckShannon.java
+++ MockMain/src/sheepdog/modules/CheckShannon.java
@@ -2,11 +2,13 @@ package sheepdog.modules;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.List;
 
 import biolockj.Constants;
 import biolockj.Log;
 import biolockj.module.BioModuleImpl;
 import biolockj.util.BioLockJUtil;
+import sheepdog.modules.util.KrakenExpectedUnclassified;
 
 public class CheckShannon extends BioModuleImpl
 {
@@ -22,22 +24,56 @@ public class CheckShannon extends BioModuleImpl
 	@Override
 	public void executeTask() throws Exception
 	{
-		
 		Log.info( getClass(), "IN stub for executeTask()");
-		
 		
 		Collection<File> pipelineInput = BioLockJUtil.getPipelineInputFiles();
 		
-		for( File f : pipelineInput )
-			Log.info( getClass(), "HELLO pipeline " + f.getAbsolutePath());
+		List<File> inputFiles = getInputFiles();
 		
-		for( File f : getInputFiles())
-			Log.info(getClass(), "HELLO input " + f.getAbsolutePath());
-		
+		for(File f : pipelineInput)
+		{
+			File matching = findMatching(f, inputFiles);
+			Log.info( getClass(), "HELLO: Compare " + f.getAbsolutePath() + " " + matching.getAbsolutePath());
+			
+			//todo actually compre the files!
+			
+			Log.info( getClass(), "HELLO:  Pass " + f.getAbsolutePath() + " " + matching.getAbsolutePath());
+			
+		}
 		
 		Log.info( getClass(), "Exit stub for executeTask() with global pass");
+	
 		
 	}
+	
+	
+
+	private static File findMatching(File pipelineFile, List<File> inputFiles) throws Exception
+	{
+		String sampleId = pipelineFile.getName();
+		sampleId = sampleId.replace("_taxaCount", "").replace(".tsv", "");
+		sampleId = sampleId + "_Shannon.tsv";
+		
+		File returnFile = null;
+		
+		for( File f : inputFiles)
+		{
+			if( f.getName().indexOf(sampleId) != -1 )
+			{
+				if( returnFile != null)
+					throw new Exception("Duplicate for " + sampleId);
+				
+				returnFile = f;
+			}
+		}
+		
+		if( returnFile == null)
+			throw new Exception("Could not find match for " + pipelineFile.getAbsolutePath());
+		
+		return returnFile;
+	}
+	
+	
 			
 	@Override
 	public String getDockerImageName() {
